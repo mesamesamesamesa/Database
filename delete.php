0a9ed4222b1def175510ef0c871f916489e11075
mesamesamesamesa <73683412+mesamesamesamesa@users.noreply.github.com>
2020-11-20 16:09:52 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -10,7 +10,7 @@ if (mysqli_connect_errno($conn))
 
 $id = $_POST["id"];
 
-$sql = "DELETE FROM guestbook WHERE id=$id";
+$sql = "DELETE FROM guestbook WHERE id='$id'";
 
 
 if (mysqli_query($conn, $sql)) 

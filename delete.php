ca2f0dd07d6a01ff39dff2480fcd0cd76d45ad82
mesamesamesamesa <73683412+mesamesamesamesa@users.noreply.github.com>
2020-11-20 13:42:16 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -10,7 +10,7 @@ if (mysqli_connect_errno($conn))
 
 $id = $_GET["id"];
 
-$sql = "DELETE FROM guestbook WHERE id = $id";
+$sql = "DELETE FROM guestbook WHERE ID = $id";
 
 
 if (mysqli_query($conn, $sql)) {

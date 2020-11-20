bed693db762012170ee9e9be8333e81278d87e33
mesamesamesamesa <73683412+mesamesamesamesa@users.noreply.github.com>
2020-11-20 17:46:25 +0700

    Add files via upload

---
+++ update.php
@@ -0,0 +1,27 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'mesa.mysql.database.azure.com', 'it63070139@mesa', 'TLSqeq45', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+$id = $_POST['id'];
+
+
+$sql = "UPDATE guestbook SET Name=$name Comment=$comment Link=$comment WHERE id=$id";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Update successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>

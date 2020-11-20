24884fb9c6d4027a3807b09886b82ee2fadf471a
mesamesamesamesa <73683412+mesamesamesamesa@users.noreply.github.com>
2020-11-20 20:03:00 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'mesa.mysql.database.azure.com', 'it63070139@mesa', 'TLSqeq45', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$id = $_GET["id"];
+
+$sql = "DELETE FROM guestbook WHERE id=$id";
+
+
+if (mysqli_query($conn, $sql)) 
+{
+  echo "Record delete successfully";
+} 
+else 
+{
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+  
+mysqli_close($conn);
+?>
